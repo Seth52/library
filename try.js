55fc74c6e0f423dcf637d6bf0f4ef7838e724d6a
Seth <25378370+Seth52@users.noreply.github.com>
2022-10-15 14:55:56 -0400

    checkbox

--- try.js
+++ try.js
@@ -1,12 +1,12 @@
-let myLibrary=[/*[title = 'The Hobbit', author='tolk'],[title='shh', author='me',t='t']*/];
+let myLibrary=[];
 
 
 // filter will remove a specific object from array
-function Book(title,author, pages,read, remove){
+function Book(title,author, pages,read){
     this.title=title
     this.author=author
     this.pages = pages
-    this.read=read
+    this.read= read
    
 }
 
@@ -16,25 +16,24 @@ function up(){
     let title = document.querySelector('#title').value;
     let author = document.querySelector('#author').value;
     let pages = document.querySelector('#pages').value;
-    let readq = document.querySelector('#read');
-    let read = readq.options[readq.selectedIndex].value;
+    let readq = document.querySelector('#read').checked;
+    let read=readq;
     
     //creates object and sends to array
     let book = new Book (title,author,pages,read); 
     myLibrary.push(book);
     
-    
-    console.log(book)
-    console.log(`${title}, by ${author}, ${pages}, ${read}`);
+
     
     
-    //test of table
+    //test of table creation
     
     const tbl = document.createElement("table");
     tbl.id='booklog';
     const tblBody = document.createElement('tbody');
     tblBody.id='bodyme';
-    //test 2
+
+    //create display of books in the table
     for(i=0; i < myLibrary.length; i++){
     const row=document.createElement('tr');
     row.setAttribute('id',`${i}book`)
@@ -55,18 +54,33 @@ function up(){
     cell.appendChild(cellText);
     row.appendChild(cell);
     
-     cell = document.createElement('td');
-    cellText = document.createTextNode(myLibrary[i].read);
-    cell.appendChild(cellText);
-    row.appendChild(cell);
     
+    // read
+
+     let reader = document.createElement('td');  
+     reader.classList.add('reader'); 
+    //cellText = document.createTextNode(myLibrary[i].read);
+    cellText=document.createElement('input');
+    cellText.type="checkbox";
+    
+   
+    function check(){
+      if(myLibrary[i].read == true){
+     cellText.checked =true;
+      }
+    };
+    check();
+    reader.appendChild(cellText);
+    row.appendChild(reader);
+    
+
+
+
     let remove = document.createElement('td');
     cellText = document.createTextNode('Remove');
     remove.classList.add('removal');
     remove.setAttribute('id',`${i}remove`)
   
-    
-    
     remove.appendChild(cellText);
     row.appendChild(remove);
     
@@ -88,7 +102,7 @@ function up(){
       document.getElementById('bodyme').remove();
       up();
     
-    // remove button
+    
    
     }
       else {
@@ -96,7 +110,29 @@ function up(){
 
       }
     })
-    
+
+
+
+
+
+
+
+
+// working on updating myLibrary array and dom 
+const did = document.querySelector('#myBooks');
+did.addEventListener('change',function(e){
+  if(e.target.className =='reader'){
+    console.log('ok');
+  }
+  
+});
+
+
+
+
+
+
+    //delete books    
     const list= document.querySelector('#testme');
     list.addEventListener('click',function(e){
         if(e.target.className == 'removal'){
@@ -110,10 +146,31 @@ function up(){
                 myLibrary.splice(i,1)
             }
            }
-        }
-    })
+                                            } 
+                                             })
+
+ Book.prototype.read = function(){
+
+      }
 
 
     //bubbling allows u to set an event listener at a higher target to
     //not have to be generated per creation
     
+
+    /* protoype function that first allows you to toggle read status on book instance 
+
+    function Book(title,author, pages,read, remove){
+    this.title=title
+    this.author=author
+    this.pages = pages
+    this.read= read
+    }
+<select name="read" id='read'>
+        <option value="yes">Yes</option>
+        <option value="no">No</option>
+    </select>
+    
+    
+
+*/
